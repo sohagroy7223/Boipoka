ef345efa86e578589f38b2423bc5d63b7e4d0e9f
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-27 22:34:27 +0600

    addad home path and about path

--- src/Routers/Routes.jsx
+++ src/Routers/Routes.jsx
@@ -3,11 +3,24 @@ import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Root from "../Pages/Root/Root";
 import Error from "../Pages/ErrorPage/Error";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "about",
+        Component: About,
+      },
+    ],
   },
 ]);
