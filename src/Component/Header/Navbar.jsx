4286bb506f9042c65293807715245367efb90085
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-27 23:04:23 +0600

    addad navber and coustomize my navber

--- src/Component/Header/Navbar.jsx
+++ src/Component/Header/Navbar.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h3>i am navbar</h3>
+    <div className="bg-blue-400 p-6">
+      <nav className=" flex justify-between w-11/12 mx-auto">
+        {/* logo */}
+        <h3 className="font-bold text-2xl">Book Vibe</h3>
+        <div className="flex gap-4 text-xl ">
+          <ul>Home</ul>
+          <ul>Listed Books</ul>
+          <ul>Pages to Read</ul>
+        </div>
+        <div>
+          <button>sign in</button>
+          <button>sign up</button>
+        </div>
+      </nav>
     </div>
   );
 };
--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../../Component/Header/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../../Component/Footer/Footer";
+import NavbarDaisy from "../../Component/Header/NavbarDaisy";
 
 const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <NavbarDaisy></NavbarDaisy>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
