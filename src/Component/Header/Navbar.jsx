c742799ed7131b5d2d9f5d4919a36387ff8d9f57
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-28 21:51:36 +0600

    addad some chsnge in navbae

--- src/Component/Header/Navbar.jsx
+++ src/Component/Header/Navbar.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { VscThreeBars } from "react-icons/vsc";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const handelChange = () => {
+    setShow(!show);
+  };
+
   const nav = (
     <div className="md:flex hidden gap-4 text-xl ">
       <ul className="text-[#131313] hover:border-2 hover:text-[#23BE0A] p-2 hover:rounded-lg ">
@@ -15,12 +21,14 @@ const Navbar = () => {
     </div>
   );
   return (
-    <div className="bg-white h-24 p-6">
+    <div className="bg-white relative h-24 p-6">
       <nav className="flex justify-between w-11/12 mx-auto">
+        <VscThreeBars />
         {/* logo */}
         <h3 className="text-[#131313] font-bold text-2xl">Book Vibe</h3>
         {nav}
-        <div className="md:hidden bg-teal-200 p-3 w-46 gap-4 text-xl ">
+
+        <div className="md:hidden   bg-teal-200 p-3 w-46 gap-4 text-xl ">
           <ul className="text-[#131313] hover:border-2 hover:text-[#23BE0A] p-2 hover:rounded-lg ">
             Home
           </ul>
