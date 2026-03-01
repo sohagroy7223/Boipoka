b745cbfbd217b54729bbd790949cc5d35ecd2f2d
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-01 13:35:57 +0600

    addad json file and banner some coustomize

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import bookImage from "../../assets/books.jpg";
 const Banner = () => {
   return (
-    <div className="flex">
-      <div>
-        <h2>Books to freshen up your bookshelf</h2>
-        <button className="btn">View The List</button>
+    <div className="flex justify-around border-2 bg-gray-200 rounded-2xl gap-2 mt-3">
+      <div className="text-center flex flex-col  w-full justify-center">
+        <h2 className="text-4xl font-bold text-black">
+          Books to freshen up your bookshelf
+        </h2>
+        <button className="btn border-none w-30 mx-auto mt-3 text-center bg-[#23BE0A]">
+          View The List
+        </button>
       </div>
-      <img src={bookImage} alt="" />
+      <img className="w-3/6 rounded-2xl" src={bookImage} alt="" />
     </div>
   );
 };
