da51796901d6f7321aa2b91be605b8db384acb3d
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-03 13:54:09 +0600

    addad tab and get data from useLoaderData

--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useLoaderData } from "react-router";
 
 const ReadList = () => {
+  const data = useLoaderData();
+  console.log(data);
+
   return (
     <div>
-      <h3>this is read list</h3>
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-lift">
+        <input
+          type="radio"
+          name="my_tabs_3"
+          className="tab"
+          aria-label="Read book"
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">
+          read book
+        </div>
+        <input
+          type="radio"
+          name="my_tabs_3"
+          className="tab"
+          aria-label="Wishlist Books"
+          defaultChecked
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">
+          Wishlist Books
+        </div>
+      </div>
     </div>
   );
 };
