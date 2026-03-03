a7bc23bcd7ff2bcf069cf96c50a32abe9abb9552
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-03 16:57:20 +0600

    addad card localstorage to read list

--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router";
 import { getBookFromStored } from "../../Utility/AddToLS";
+import Book from "../Book/Book";
 
 const ReadList = () => {
   const [readList, setReadList] = useState([]);
@@ -13,28 +14,34 @@ const ReadList = () => {
     const myListBook = data.filter((book) =>
       convertedStorBook.includes(book.bookId),
     );
+
     setReadList(myListBook);
-  }, []);
+  }, [data]);
 
   return (
     <div>
       {/* name of each tab group should be unique */}
-      <div className="tabs tabs-lift">
+      <div className="tabs tabs-4">
         <input
           type="radio"
           name="my_tabs_3"
           className="tab"
           aria-label="Read book"
+          defaultChecked
         />
         <div className="tab-content bg-base-100 border-base-300 p-6">
           read book : {readList.length}
+          <div className="grid lg:grid-cols-3 md:grid-cols-2">
+            {readList.map((b) => (
+              <Book key={b.bookId} book={b}></Book>
+            ))}
+          </div>
         </div>
         <input
           type="radio"
           name="my_tabs_3"
           className="tab"
           aria-label="Wishlist Books"
-          defaultChecked
         />
         <div className="tab-content bg-base-100 border-base-300 p-6">
           Wishlist Books
